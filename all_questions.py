978b8e43db182fc1764cb7c7257b16dec925ae88
erlebach <gordon.erlebach@gmail.com>
2025-01-05 23:22:19 -0500

    Solution ready for grading.

--- all_questions.py
+++ all_questions.py
@@ -3,36 +3,49 @@ Work with DENCLUE clustering.
 Do not use global variables!
 """
 
-import matplotlib.pyplot as plt
-from numpy.typing import NDArray
-import utils
 import pickle
+from typing import Any
 
-######################################################################
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import NDArray
+
 #####     CHECK THE PARAMETERS     ########
 ######################################################################
 
 
-def all_questions() -> None:
-
+def all_questions() -> dict[str, Any]:
+    """Solve all questions."""
     answers = {}
 
     # Return the sum of 3 and 7
-    answers["q1"] = None
+    answers["q1"] = 7
 
     # Return the first 8 Fibonacci numbers, starting with 0
-    answers["q2"] = None
+    answers["q2"] = [0, 1, 1, 2, 3, 5, 8, 13]
 
     # Return a plot of y versus x, where
-    # x = [0, 1, 2, 3, 4, 5]
-    # y = [0, 1, 4, 9, 16, 25]
-    answers["plot_original_cluster"] = None
+    x = [0, 1, 2, 3, 4, 5]
+    y = [0, 1, 4, 9, 16, 25]
+    answers["plot_original_cluster"] = plt.plot(x, y)
 
-    # Create a scattergram with 300 points. 
+    # Create a scattergram with 300 points.
     # 1) N(1, 2)  (standard deviation= 2) with 100 points
     # 2) N(2, 3) with 200 points
-    # Compute the mean and standard deviation of all the points. 
-    answers["Two gaussians"] = None
+    # Compute the mean and standard deviation of all the points.
+
+    # Generating 100 points from N(1, 2), where 1 is the mean and 2 is the standard deviation
+    points1 = np.random.normal(1, 2, 100)
+    points2 = np.random.normal(2, 3, 200)
+    # combine the two arrays into a single array of 300 points
+    points = np.concatenate((points1, points2))
+    # Compute the mean and standard deviation of all the points
+    mean = np.mean(points)
+    std = np.std(points)
+
+    # Plotting the generated points
+
+    answers["Two gaussians"] = [mean, std]
 
     return answers
 
